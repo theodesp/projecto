f427d66a92193c0ac561add0e8c98ad9767482c3
Theo <theo.despoudis@teckro.com>
2019-06-21 10:12:12 +0100

    Add SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import '@elastic/eui/dist/eui_theme_light.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
